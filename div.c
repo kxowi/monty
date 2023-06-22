b2227e7276f965d05653d1dd537b0f2d0e4fa081
kxowi <karim-xowi@live.fr>
2023-06-22 09:07:38 -0700

    div updated

--- div.c
+++ div.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * div - divide second element of stack by top element
+ * m_div - divide second element of stack by top element
  * @stack: double pointer to head of stack
  * @line_number: line number of current operation
  *
