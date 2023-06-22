2f6677d857021cd9c477a801302afbd021ac8a04
kxowi <karim-xowi@live.fr>
2023-06-22 05:27:39 -0700

    LIFO, FIFO project

---
+++ queue.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * queue - sets the format of the date to a queue (FIFO)
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: script line number
+ *
+ * Return: void
+ */
+void m_queue(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	var.queue = 1;
+}i
