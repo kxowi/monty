9d243abf42502a116737ff4e899afc8d655f53d0
kxowi <karim-xowi@live.fr>
2023-06-22 09:32:34 -0700

    gcc compiled

--- queue.c
+++ queue.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * queue - sets the format of the date to a queue (FIFO)
+ * m_queue - sets the format of the date to a queue (FIFO)
  * @stack: double pointer to the beginning of the stack
  * @line_number: script line number
  *
@@ -11,4 +11,4 @@ void m_queue(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 	var.queue = 1;
-}i
+}
--- sub.c
+++ sub.c
@@ -1,27 +1,29 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
 
 /**
- * _sub -  substracts the first two nodes of the stack
- * @stack: stack given by main
- * @line_cnt: line counter
+ * m_sub - subtruct top element of stack from next element and push result
+ * @stack: double pointer to the head of stack
+ * @line_number: line number of current operation
  *
  * Return: void
  */
-void _sub(stack_t **stack, unsigned int line_cnt)
+void m_sub(stack_t **stack, unsigned int line_number)
 {
-	int result;
-
-	if (!stack || !*stack || !((*stack)->next))
+	int n, m, flag;
+	
+	if (var.stack_len < 2)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_cnt);
+		dprintf(STDOUT_FILENO,
+			"L%u: can't sub, stack too short\n",
+			line_number);
 		exit(EXIT_FAILURE);
 	}
-
-	result = ((*stack)->next->n) - ((*stack)->n);
-	pop(stack, line_cnt);/*For top node*/
-	(*stack)->n = result;
+	n = (*stack)->n;
+	m_pop(stack, line_number);
+	m = (*stack)->n;
+	m_pop(stack, line_number);
+	flag = var.queue;
+	var.queue = 0;
+	m_push2(stack, m - n);
+	var.queue = flag;
 }
